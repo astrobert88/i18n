e3c6ed077cf249b842dbd7b0ca0d34e9bd086e04
Robert Pieper <astrobert@gmail.com>
2020-11-20 10:56:00 +0100

    Groomed

--- src/test/java/com/piecon/i18n/DbI18NProviderTest.java
+++ src/test/java/com/piecon/i18n/DbI18NProviderTest.java
@@ -30,55 +30,56 @@ public class DbI18NProviderTest {
 
         List<Locale> locales = dbI18NProvider.getProvidedLocales();
         Assert.assertNotNull(locales);
-        Assert.assertTrue(locales.size() > 1);
+        Assert.assertEquals(3, locales.size());
     }
 
     @Test
-    public void testGetTranslation_noKey() {
-        log.info("testGetTranslation_noKey()");
+    public void testGetTranslation_noKeyMatch() {
+        log.info("testGetTranslation_noKeyMatch()");
 
-        Locale aLocale = new Locale.Builder().setLanguage("").setRegion("").build();
-        String uiTextValue = dbI18NProvider.getTranslation("no.such.key", aLocale);
-        log.info("no.such.key="+uiTextValue);
-        Assert.assertNotNull(uiTextValue);
-        Assert.assertEquals("no.such.key", uiTextValue);
+        // Create locale. Language and country not used by this test
+        Locale aLocale = new Locale.Builder().build();
+        String translation = dbI18NProvider.getTranslation("no.such.key", aLocale);
+        Assert.assertNotNull(translation);
+        // translation should be set to the key itself.
+        Assert.assertEquals("no.such.key", translation);
     }
 
     @Test
-    public void testGetTranslation_noLanguageMatch() {
-        log.info("testGetTranslation_noLanguageMatch()");
+    public void testGetTranslation_keyMatch_noLanguageMatch() {
+        log.info("testGetTranslation_keyMatch_noLanguageMatch()");
 
-        Locale aLocale = new Locale.Builder().setLanguage("").setRegion("").build();
+        // No entry for Zulu language...
+        Locale aLocale = new Locale.Builder().setLanguage("zu").build();
         String uiTextValue = dbI18NProvider.getTranslation("home.welcome", aLocale);
-        log.info("home.welcome="+uiTextValue);
         Assert.assertNotNull(uiTextValue);
         Assert.assertEquals("home.welcome", uiTextValue);
     }
 
     @Test
-    public void testGetTranslation_languageMatchButNoCountryMatch() {
-        log.info("testGetTranslation_languageMatchButNoCountryMatch()");
+    public void testGetTranslation_keyMatch_languageMatch_noCountryMatch() {
+        log.info("testGetTranslation_keyMatch_languageMatch_noCountryMatch()");
 
+        // Switzerland (CH) not in test data
         Locale aLocale = new Locale.Builder().setLanguage("de").setRegion("CH").build();
         String uiTextValue = dbI18NProvider.getTranslation("home.welcome", aLocale);
-        log.info("home.welcome="+uiTextValue);
         Assert.assertNotNull(uiTextValue);
-        // There is no country match for Switzerland (CH) but there is a German one without a country code so return that.
+        // There is no country match for Switzerland (CH) but there is a match for the German language without a
+        // country code so return that.
         Assert.assertEquals("Wilkommen", uiTextValue);
     }
 
     @Test
     // Almost identical to previous test but now the first entry to match the language has the wrong country code.
     // Even though there is one without a country code further down. It should return the one without a country code.
-    public void testGetTranslation_languageMatchButNoCountryMatch2() {
-        log.info("testGetTranslation_languageMatchButNoCountryMatch2()");
+    public void testGetTranslation_keyMatch_languageMatch_noCountryMatch2() {
+        log.info("testGetTranslation_keyMatch_languageMatch_noCountryMatch2()");
 
+        // No entry for CA and the first entry found with matching language is AU. However, further down the table,
+        // there is an entry with no country code set. That one should be returned instead.
         Locale aLocale = new Locale.Builder().setLanguage("en").setRegion("CA").build();
         String uiTextValue = dbI18NProvider.getTranslation("hello", aLocale);
-        log.info("hello="+uiTextValue);
         Assert.assertNotNull(uiTextValue);
-        // There is no entry for Canada (CA) and the first entry with 'en' as language is AU. However, further down
-        // the list there is one with country code ''. That's the one we expect
         Assert.assertEquals("Hello", uiTextValue);
     }
 
@@ -86,33 +87,34 @@ public class DbI18NProviderTest {
     public void testGetTranslation() {
         log.info("testGetTranslation()");
 
+        // English, no country
         Locale aLocale = new Locale.Builder().setLanguage("en").setRegion("").build();
         String uiText = dbI18NProvider.getTranslation("home.welcome", aLocale);
-        log.info("home.welcome="+uiText);
         Assert.assertNotNull(uiText);
         Assert.assertEquals("Welcome", uiText);
 
+        // English, no country. First entry found is US but should still return the entry without a country code
         aLocale = new Locale.Builder().setLanguage("en").setRegion("").build();
         uiText = dbI18NProvider.getTranslation("home.favcolor", aLocale);
-        log.info("home.favcolor="+uiText);
         Assert.assertNotNull(uiText);
         Assert.assertEquals("My favourite colour is blue", uiText);
 
+        // English, US. Expect the entry with US spelling ie. 'favorite' instead of 'favourite',
+        // 'color' instead of 'colour'
         aLocale = new Locale.Builder().setLanguage("en").setRegion("US").build();
         uiText = dbI18NProvider.getTranslation("home.favcolor", aLocale);
-        log.info("home.favcolor="+uiText);
         Assert.assertNotNull(uiText);
         Assert.assertEquals("My favorite color is blue", uiText);
 
+        // German, no country. Expect German version of 'home.welcome'
         aLocale = new Locale.Builder().setLanguage("de").setRegion("").build();
         uiText = dbI18NProvider.getTranslation("home.welcome", aLocale);
-        log.info("home.welcome="+uiText);
         Assert.assertNotNull(uiText);
         Assert.assertEquals("Wilkommen", uiText);
 
+        // Chinese, no country. Expect Chinese version of 'home.welcome'
         aLocale = new Locale.Builder().setLanguage("zh").setRegion("").build();
         uiText = dbI18NProvider.getTranslation("home.welcome", aLocale);
-        log.info("home.welcome="+uiText);
         Assert.assertNotNull(uiText);
         Assert.assertEquals("歡迎", uiText);
     }
--- src/test/resources/data-H2.sql
+++ src/test/resources/data-H2.sql
@@ -1,8 +1,8 @@
 INSERT INTO ui_text (language_code, country_code, text_key, text_value) VALUES
 ('en', '', 'home.welcome','Welcome'),
 ('en', '', 'home.info','This page is displayed in English.'),
-('en', '', 'home.favcolor','My favourite colour is blue'),
 ('en', 'US', 'home.favcolor','My favorite color is blue'),
+('en', '', 'home.favcolor','My favourite colour is blue'),
 ('en', '', 'home.changelanguage','Supported languages : '),
 ('en', '', 'home.lang.en','English'),
 ('en', '', 'home.lang.de','German'),
